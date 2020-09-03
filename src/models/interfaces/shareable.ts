123e7a0b49bf5fd43e9bee17aaa807495108aed6
Wilkinson Balce <wilkinson.balce@gmail.com>
2020-09-04 01:16:23 +1000

    chore (models): remove commented out code

--- src/models/interfaces/shareable.ts
+++ src/models/interfaces/shareable.ts
@@ -1,5 +1,4 @@
 import { IBaseModel } from './baseModel';
-// import { IItemDataRetrievable } from './itemDataRetrievable';
 
 export interface IShareable extends IBaseModel {
     share: (connectionId: string) => Promise<any>;
