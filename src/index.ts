7d4667104827e3efbe3efde77174eff5c1d9fa21
Wilkinson Balce <wilkinson.balce@gmail.com>
2020-09-06 22:09:45 +1000

    fix (index): fix import name for app icon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { AppItem } from './components/app-item';
+import { AppIcon } from './components/app-icon';
 import { AppGallery } from './components/app-gallery';
 import { AppLogin } from './components/app-login';
 import { AppItemModal } from './components/app-item-modal';
@@ -7,15 +7,17 @@ import { AppItemSharedCheckbox } from './components/app-item-shared-checkbox';
 import './index.scss';
 
 window.customElements.define('app-login', AppLogin);
-window.customElements.define('app-item', AppItem);
+window.customElements.define('app-icon', AppIcon);
 window.customElements.define('app-gallery', AppGallery);
 window.customElements.define('app-item-modal', AppItemModal);
 window.customElements.define('app-item-shared-checkbox', AppItemSharedCheckbox);
 window.customElements.define('app-connections', AppConnections);
 
-document.getElementById('app').innerHTML = `
+const appEl = document.getElementById('app');
+
+appEl.innerHTML = `
     <app-login></app-login>
-    <app-gallery></app-gallery>
     <app-item-modal></app-item-modal>
+    <app-gallery></app-gallery>
     <app-connections></app-connections>
 `;
