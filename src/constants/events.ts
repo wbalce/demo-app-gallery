26fce1ff740fd7a5e04ce5f750167c06d4162212
Wilkinson Balce <wilkinson.balce@gmail.com>
2020-09-06 22:10:46 +1000

    feat (constants): add more events

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -1,7 +1,13 @@
 export const Events = {
     LOGIN_SUCCESSFUL: 'login:successful',
     LOGIN_FAILED: 'login:failed',
-    ITEM_CLICKED: 'item:clicked',
+    LOGIN_OP_STARTED: 'loginOperation:started',
+    LOGIN_OP_ENDED: 'loginOperation:ended',
+    ICON_CLICKED: 'icon:clicked',
+    SHARE_CLICKED: 'share:clicked',
     SHARE_OP_STARTED: 'shareOperation:started',
-    SHARE_OP_ENDED: 'shareOperation:ended'
+    SHARE_OP_ENDED: 'shareOperation:ended',
+    CONNECTIONS_RECEIVED: 'connections:received',
+    ITEM_DATA_RECEIVED: 'itemData:recieved',
+    ITEM_DATA_RETRIEVABLE_IDS_RECEIVED: 'itemDataRetreivableIds:recieved'
 }
